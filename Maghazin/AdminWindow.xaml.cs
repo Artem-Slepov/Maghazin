14e25891f98be307375abd34f756f43691833a0f
Student_22_01 <Student_22_01@PC_STUD_22_01>
2022-02-11 14:28:40 +0300

    Добавьте файлы проекта.

---
+++ Maghazin/AdminWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Navigation;
+
+namespace Maghazin
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNNazad_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mm = new MainWindow();
+            mm.Show();
+            Close();
+        }
+       
+        private void BTNMonetki_Click(object sender, RoutedEventArgs e)
+        {
+            MoyaRamka.Navigate(new Coins());
+             
+        }
+
+        private void BTNVoda_Click(object sender, RoutedEventArgs e)
+        {
+            MoyaRamka.Navigate(new Drinks());
+        }
+
+        private void BTNOtchet_Click(object sender, RoutedEventArgs e)
+        {
+            MoyaRamka.Navigate(new Otchet());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mm = new MainWindow();
+            mm.Show();
+            Close();
+        }
+    }
+}
---
+++ Maghazin/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maghazin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        int kaka1; 
+        internal int coins;
+        
+        public static int vor1;
+        public static int vor2;
+        public static int vor3;
+        public static int vor4;
+
+        Coins coni = new Coins();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTNAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (Parol.Text == "1234") 
+                {
+                    AdminWindow ad = new AdminWindow();
+                    ad.Show();
+                    Close();
+                }
+                else if (Parol.Text == null)
+                {
+                    MessageBox.Show("Введи хоть что-нибудь");
+                }
+                else
+                {
+                    MessageBox.Show("Неверный код");
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+
+        }
+
+
+        private void BTN1_Click(object sender, RoutedEventArgs e)
+        {
+            kaka1++;
+            TXVvod.Text = kaka1.ToString();
+
+        }
+
+        private void BTN2_Click(object sender, RoutedEventArgs e)
+        {
+            kaka1 = kaka1 + 2;
+            TXVvod.Text = kaka1.ToString();
+            
+        }
+
+        private void BTN10_Click(object sender, RoutedEventArgs e)
+        {
+            kaka1 = kaka1 + 10;
+            TXVvod.Text = kaka1.ToString();
+           
+        }
+
+        private void BTN5_Click(object sender, RoutedEventArgs e)
+        {
+            kaka1 = kaka1 + 5;
+            TXVvod.Text = kaka1.ToString();
+            
+        }
+    }
+}
